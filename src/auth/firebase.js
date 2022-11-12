ea307c162492987f5bc671343fd8f0d516e3542b
Birol ALKAN <birolalkan93@gmail.com>
2022-11-12 23:36:50 +0300

    logout

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 //* Your web app's Firebase configuration
 // TODO: Replace the following with your app's Firebase project configuration
@@ -17,12 +17,39 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createuser = async(email, password) => {
+export const createuser = async(email, password, navigate) => {
   try {
    let userCredential= await createUserWithEmailAndPassword(auth, email, password)
+   navigate("/");
    console.log(userCredential)
   } catch (error) {
-    console.log(error.message);
+    alert(error.message);
   }
- 
+}
+
+
+export const signIn =  async(email, password, navigate) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password)
+    navigate("/")
+  } catch (error) {
+    alert(error.massage)
+  }
+};
+
+export const userObserver = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      // ...
+    } else {
+     console.log("user signed out");
+    }
+  });
+};
+
+export const Logout = () => {
+  signOut(auth)
 }
